e238e3bb2c16c758091bb1b9770bb8ce19d071a6
William Redenbaugh <wmredenbaugh@gmail.com>
2023-09-11 15:20:40 -0700

    removing all the cmake files, adding to gitignore

--- CMakeFiles/PosixHalExample.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/PosixHalExample.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/wredenba/git/PosixHALExample/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
--- CMakeFiles/PosixHalExample.dir/src/LinuxCSAL/CSAL_SHARED/statemachine.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/PosixHalExample.dir/src/LinuxCSAL/CSAL_SHARED/statemachine.cpp.o: \
- /home/wredenba/git/PosixHALExample/src/LinuxCSAL/CSAL_SHARED/statemachine.cpp \
- /usr/include/stdc-predef.h \
- /home/wredenba/git/PosixHALExample/src/LinuxCSAL/CSAL_SHARED/statemachine.h \
- /home/wredenba/git/PosixHALExample/src/LinuxCSAL/CSAL_SHARED/../../enabled_modules.h
